e7fe2825332413c3054e94f1e463d1b36cc5b859
Javier <you@example.com>
2022-07-13 14:19:02 -0300

    extra

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -70,6 +70,17 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var capi = numero.toString();
+  var numAstring = "";
+
+  for (var i = capi.length -1; i >= 0; i--) {
+    numAstring += capi[i];
+  }
+  if (capi === numAstring) {
+    return "Es capicua";
+  } else {
+    return "No es capicua"
+  }
 }
 
 
@@ -77,6 +88,14 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var Abc = "";
+
+  for (var i = 0; i < cadena.length; i++) {
+    if (cadena[i] !== "a" && cadena[i] !== "b" && cadena[i] !== "c")
+      Abc += cadena[i];
+
+  }
+    return  Abc;
 }
 
 
